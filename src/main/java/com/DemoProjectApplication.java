d35698e4a31167d057629a4c68c20eb2e8dfeef2
Hemant Maurya <68320014+hemantmaurya344@users.noreply.github.com>
2024-01-24 12:56:38 +0530

    Update DemoProjectApplication.java

--- src/main/java/com/DemoProjectApplication.java
+++ src/main/java/com/DemoProjectApplication.java
@@ -2,32 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-@EnableDiscoveryClient
-@EnableFeignClients
 public class DemoProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoProjectApplication.class, args);
-<<<<<<< HEAD
         System.out.println("Demo is Running");
-=======
-        JenkinsClass ob = new JenkinsClass();
-        ob.demo();
-        JenkinsAnotherClass ob1 = new JenkinsAnotherClass();
-        ob1.temp();
-        System.out.println("Hello i am Aniket");
-        System.out.println("Hello i am Aniket 2");
-        System.out.println("Hello i am Girjesh");
-        System.out.println("Hello i am Jenkins");
-
-
->>>>>>> a37a4f4854c62febca1687b1a508ab05a19d47e8
-
     }
-
-
 }
