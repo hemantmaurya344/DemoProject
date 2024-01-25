09e271e1b7bab579810dc2d4899dc17a05c51162
Hemant Maurya <68320014+hemantmaurya344@users.noreply.github.com>
2024-01-25 12:16:40 +0530

    Update DemoController.java

--- src/main/java/com/controller/DemoController.java
+++ src/main/java/com/controller/DemoController.java
@@ -1,7 +1,6 @@
 package com.controller;
 
 import com.Entity.User;
-import com.netflix.discovery.converters.Auto;
 import com.repo.UserRepo;
 import org.apache.http.HttpStatus;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,29 +17,24 @@ public class DemoController {
     private UserRepo userRepo;
 
     @GetMapping("/")
-    public String demo1()
-    {
-        return " Demo Project " + "   Jai Shree Ram";
-
+    public String demo1()    {
+        return " Demo Project " + "Working";
     }
 
     @PostMapping("/addUser")
-    public ResponseEntity<?> addUser(@RequestBody User user)
-    {
+    public ResponseEntity<?> addUser(@RequestBody User user){
         User user1 = userRepo.save(user);
         return ResponseEntity.status(HttpStatus.SC_OK).body(user1);
     }
 
     @GetMapping("/getAll")
-    public ResponseEntity<?> demo2()
-    {
+    public ResponseEntity<?> demo2(){
         List<User> user1 = userRepo.findAll();
         return ResponseEntity.status(HttpStatus.SC_OK).body(user1);
     }
 
     @GetMapping("/getById/{id}")
-    public ResponseEntity<?> getById(@PathVariable int id)
-    {
+    public ResponseEntity<?> getById(@PathVariable int id){
         Optional<User> user1 = userRepo.findById(id);
         return ResponseEntity.status(HttpStatus.SC_OK).body(user1);
     }
